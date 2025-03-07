e3d2d1498532e15053fab703e0a32144f6ea484d
godriele11@yahoo.com <godriele11@yahoo.com>
2025-03-07 12:48:39 -0800

    Changed the Cognito User pool sign-in options

--- app/backend/app/app.py
+++ app/backend/app/app.py
@@ -2,53 +2,45 @@ from flask import Flask, redirect, url_for, session
 from authlib.integrations.flask_client import OAuth
 import os
 
-# Initialize the Flask app
 app = Flask(__name__)
 app.secret_key = os.urandom(24)  # Use a secure random key in production
-
-# Initialize OAuth
 oauth = OAuth(app)
 
-# Register the OAuth app with AWS Cognito
 oauth.register(
-    name='oidc',
-    authority='https://cognito-idp.us-east-1.amazonaws.com/us-east-1_TcqiaNQSu',
-    client_id='3snvum8hd990384u743joh3edt',
-    client_secret='1nt3m52f5s89iku57heu487ptdfsk0ftgelnlk8vopik9jek4o2m',
-    server_metadata_url='https://cognito-idp.us-east-1.amazonaws.com/us-east-1_TcqiaNQSu/.well-known/openid-configuration',
-    client_kwargs={'scope': 'email openid phone'}
+  name='oidc',
+  authority='https://cognito-idp.us-east-1.amazonaws.com/us-east-1_Tee7jWEoJ',
+  client_id='33o5i38p2n96fofth2jflj1ui',
+  client_secret='kllmi8gtvo81khtn84kp7rq37k7068pea85rlkakig031mn37b6',
+  server_metadata_url='https://cognito-idp.us-east-1.amazonaws.com/us-east-1_Tee7jWEoJ/.well-known/openid-configuration',
+  client_kwargs={'scope': 'phone openid email'}
 )
 
-# Routes
 @app.route('/')
 def index():
-    """Home route, checks if user is logged in."""
     user = session.get('user')
     if user:
-        return f'Hello, {user["email"]}. <a href="/logout">Logout</a>'
+        return  f'Hello, {user["email"]}. <a href="/logout">Logout</a>'
     else:
         return f'Welcome! Please <a href="/login">Login</a>.'
-
+    
 @app.route('/login')
 def login():
-    """Initiates login process and redirects to Cognito."""
-    redirect_uri = url_for('authorize', _external=True)
-    return oauth.oidc.authorize_redirect(redirect_uri)
+    # Alternate option to redirect to /authorize
+    # redirect_uri = url_for('authorize', _external=True)
+    # return oauth.oidc.authorize_redirect(redirect_uri)
+    return oauth.oidc.authorize_redirect('https://d84l1y8p4kdic.cloudfront.net')
 
 @app.route('/authorize')
 def authorize():
-    """Handles the callback from Cognito after successful authentication."""
     token = oauth.oidc.authorize_access_token()
-    user = oauth.oidc.parse_id_token(token)
+    user = token['userinfo']
     session['user'] = user
     return redirect(url_for('index'))
 
 @app.route('/logout')
 def logout():
-    """Logs the user out by clearing the session."""
     session.pop('user', None)
     return redirect(url_for('index'))
 
-# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
